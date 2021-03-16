1fc2818c0a4e126409a96554db849039093036f7
jrossyy33 <jazzi@Jasmines-MacBook-Air.local>
2021-03-15 20:28:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from base import vector, square
 food = vector(0, 0)
 snake = [vector(10, 0)]
 speed = 10
-aim = vector(0,-speed)
+aim = vector(0, -speed)
 
 def change(x, y):
-    "change snake direction"
+    ''' change snake direction '''
     aim.x = x
     aim.y = y
 def inside(head):
-    "return True if head is inside the boundary"
+    '''return True if head is inside the boundary'''
     return -200 < head.x < 190 and -200 < head.y < 190
     # ((-200,190),(-200,190)
 
 def move():
-    "move snake forward"
+    ''' move snake forward '''
     head = snake[-1].copy()
     head.move(aim)
 
@@ -28,7 +28,7 @@ def move():
 
     if head == food:
         print("snake point: ", len(snake))
-        food.x = randrange(-15,15) * 10
+        food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
         snake.pop(0)
@@ -39,15 +39,16 @@ def move():
     update()
     ontimer(move, 100)
 
+
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
-#event handling
+
+''' event handling '''
 onkey(lambda: change(speed, 0), 'Right')
-onkey(lambda: change(-speed,0), 'Left')
-onkey(lambda: change(0,speed), 'Up')
-onkey(lambda: change(0,-speed), 'Down')
+onkey(lambda: change(-speed, 0), 'Left')
+onkey(lambda: change(0, speed), 'Up')
+onkey(lambda: change(0, -speed), 'Down')
 move()
 done()
-
